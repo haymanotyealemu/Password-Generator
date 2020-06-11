13153bcedd239c31a12f187fe9b32fd2f7f6edf6
Haymanot W <haymanotalemu21@gmail.com>
2020-06-11 18:05:08 -0400

    adding codes

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// WHEN a user click the button to generate a password, series of prompt will display.
+  // Assignment Code + event listener
+  document.querySelector("#generate").addEventListener("click" , writePassword);
+
+  // Make an Array for each character types.
+  var alphaLower = [function getRandomLower() { 
+    return String.fromCharCode(Math.floor(Math.random()*26) + 97);
+    }];
+  var alphaUpper = [function getRandomUpper() { 
+  return String.fromCharCode(Math.floor(Math.random()*26) + 65);
+    }];
+var numeric = [function getRandomNumber() { 
+      return String.fromCharCode(Math.floor(Math.random()*10) + 48);
+    }];
+var specialCharacter = [function getRandomSpecialCharacter() { 
+    var symbols = '!#$%&*+ (), -./:;<=>?@[]^_`{|}~';
+    return symbols[Math.floor(Math.random()* symbols.length)];
+  }];
+      
+// Variable Declaration
+
+var confirmLength = "";
+var confirmlowercase;
+var confirmuppercase;
+var confirmnumber;
+var confirmspecialcharacter;
+
+// Ask for length of password
+var lengthPass = prompt("How many characters will your password be? The length of the password must  have 8 character and not morethan 128 character");
+function  generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to be?"));
+  // loop length of password times.
+  while (confirmLength <= 7 || confirmLength >= 129){
+          alert("Password length must have atleast 8 character and not morethan 128 character. Try again!" );
+          var confirmLength = (prompt("How many characters would you like your password to be?"));
+  }
+  // Identify which character type include in the password.
+  var confirmlowercase = confirm("Click Ok if you would like to include lowercase character ?");
+  var confirmuppercase = confirm("Click Ok if you would like to include uppercase character ?");
+  var confirmnumber = confirm("Click Ok if you would like to include numeric character ?");
+  var confirmspecialcharacter = confirm("Click Ok if u would like to include special character ?");
+}
 
 
 
@@ -15,37 +57,10 @@ function writePassword() {
    // Ask for length of password
     var lengthPass = prompt("How many characters will your password be? The length of the password must have 8 character and not morethan 128 character");
 
-    //Ask for character type
-    var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
-
-    var lower = prompt("Enter a lowercase character type");
-    var upper = prompt("Enter a uppercase character type");
-
-    var numeric = prompt("Enter a numeric character type");
-    var special =  prompt("Enter specialcharacter type");
 
 //generate password 
 // Generator functions - http://www.net-comber.com/charset.html
-function generatePassword() {
-  //evaluate character type
-  var charSet = "";
-  var lower = function getRandomLower() { 
-    return String.fromCharCode(Math.floor(Math.random()*26) + 97);
-    }
-  var upper = function getRandomUpper() { 
-  return String.fromCharCode(Math.floor(Math.random()*26) + 65);
-    }
-  var numeric = function getRandomNumber() { 
-      return String.fromCharCode(Math.floor(Math.random()*10) + 48);
-    }
-  var special = function getRandomSpecialCharacter() { 
-    var symbols = '!#$%&*+ (), -./:;<=>?@[]^_`{|}~';
-    return symbols[Math.floor(Math.random()* symbols.length)];
-  }
-  
-  
 
-}
 
 
 
