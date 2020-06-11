fca89aaa1bb15f977ded58e12eebba0af75a3b41
Haymanot W <haymanotalemu21@gmail.com>
2020-06-11 08:41:21 -0400

    prompt coding

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 
+
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
@@ -10,64 +11,47 @@ function writePassword() {
     passwordText.value = password;
   
   }
-  // Generator functions - http://www.net-comber.com/charset.html
+  
    // Ask for length of password
-    var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
-    //ask for character type
+    var lengthPass = prompt("How many characters will your password be? The length of the password must have 8 character and not morethan 128 character");
+
+    //Ask for character type
     var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
 
-//generate password
+    var lower = prompt("Enter a lowercase character type");
+    var upper = prompt("Enter a uppercase character type");
+
+    var numeric = prompt("Enter a numeric character type");
+    var special =  prompt("Enter specialcharacter type");
+
+//generate password 
+// Generator functions - http://www.net-comber.com/charset.html
 function generatePassword() {
   //evaluate character type
   var charSet = "";
-  var charTypeLower = charType.toLowerCase();
-  if( charTypeLower === "lowercase" ) {
-    charSet = "abcdefghijklmnopqrstuvwxyz";
-  } else if( charTypeLower === "uppercase" ) {
-    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } else if( charTypeLower === "numeric" ) {
-    charSet = "0123456789";
-  } else if( charTypeLower === "special" ) {
-    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  } 
-  //return value
-  var retVal = "";
-  for (var i = 0; i < length; i++) {
-    //picks a character within charSet at index of random number
-    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
-  }
-  return retVal;
-}
-alert(generatePassword());
-
-
-
-
-
-
-
-
-
-
-  function getRandomLower() { 
+  var lower = function getRandomLower() { 
     return String.fromCharCode(Math.floor(Math.random()*26) + 97);
-}
-
-
-function getRandomUpper() { 
-    return String.fromCharCode(Math.floor(Math.random()*26) + 65);
-}
-
-function getRandomNumber() { 
-    return String.fromCharCode(Math.floor(Math.random()*10) + 48);
-}
-function getRandomSpecialCharacter() { 
+    }
+  var upper = function getRandomUpper() { 
+  return String.fromCharCode(Math.floor(Math.random()*26) + 65);
+    }
+  var numeric = function getRandomNumber() { 
+      return String.fromCharCode(Math.floor(Math.random()*10) + 48);
+    }
+  var special = function getRandomSpecialCharacter() { 
     var symbols = '!#$%&*+ (), -./:;<=>?@[]^_`{|}~';
     return symbols[Math.floor(Math.random()* symbols.length)];
+  }
+  
+  
+
 }
 
 
- 
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
