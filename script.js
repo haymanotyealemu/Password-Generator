db9094464e98727497d9906446654b915a2d51d4
Haymanot W <haymanotalemu21@gmail.com>
2020-06-11 23:09:09 -0400

    final-commit

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ var generateBtn = document.querySelector("#generate");
   // Make an Array for each character types.
   var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
     ];
+
   var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 var specialCharacter = ["!", "#", "$", "%", "&", "*", "+", ".", "@", "/", ":", ";", "<", "=", ">", "?", "[", "]", "^", "_", "`", "{", "|", "}", "~", "'", ];
     
   
@@ -21,15 +24,21 @@ var confirmuppercase;
 var confirmnumber;
 var confirmspecialcharacter;
 
-// Ask for length of password
+// Generate Password Function.
 
 function  generatePassword() {
-  var confirmLength = Number((prompt("How many characters would you like your password to be?")));
+
+  // Ask for length of password
+
+  var confirmLength = (prompt("How many characters would you like your password to be?"));
+
   // loop length of password times.
+
   while (confirmLength <= 7 || confirmLength >= 129){
-          alert("Password length must have atleast 8 character and not morethan 128 character. Try again!" );
+          alert("Password length must have atleast 8 character and not more than 128 character. Try again!" );
           var confirmLength = (prompt("How many characters would you like your password to be?"));
   }
+  
   // Identify which character type include in the password.
   var confirmlowercase = confirm("Click Ok if you would like to include lowercase character ?");
   var confirmuppercase = confirm("Click Ok if you would like to include uppercase character ?");
@@ -45,7 +54,7 @@ function  generatePassword() {
   var confirmnumber = confirm("Click Ok if you would like to include numeric character ?");
   var confirmspecialcharacter = confirm("Click Ok if u would like to include special character ?");
   }
-  //Determine the password character.
+  //Determine the password character and using the concat() method joins two or more strings.
   var passwordCharacters = [];
   if (confirmlowercase) {
     passwordCharacters = passwordCharacters.concat(alphaLower);
@@ -60,38 +69,22 @@ function  generatePassword() {
     passwordCharacters = passwordCharacters.concat(specialCharacter);
   }
   console.log( passwordCharacters);
-  // loop for a randompassword character from array.
+  // loop for a random password character from array.
 
   var randomPassword = ""
   for (i=0; i< confirmLength; i++) {
     var randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random()*passwordCharacters.length)];
     console.log(randomPassword );
   }
+  return randomPassword;
 }
-
-
 
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
   
-    passwordText.value = password;
+    passwordText.value = password;;
   
   }
-  
-   
 
-
-//generate password 
-// Generator functions - http://www.net-comber.com/charset.html
-
-
-
-
-
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
